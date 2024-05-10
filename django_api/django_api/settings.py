62fb511409aea11711315c14a44d18ec2952ee16
Octavio Montt <63523896+Chacon-tu-drema@users.noreply.github.com>
2024-05-10 13:51:11 +0100

    docker compose django-api rest app

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -24,11 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-78ch)m1-60(tf#!)=8mqog$)3xzs(trhj8^q!+jg@pp&@3%%3@"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
